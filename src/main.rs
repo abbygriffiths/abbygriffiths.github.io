32b25a16849a869967f50f02ee647fd131bb63e6
Abirbhav Goswami <abirbhav.goswami@naukri.com>
2023-10-26 11:55:08 +0530

    use cleaner more rust-like syntax

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,59 @@ extern crate rocket;
 #[macro_use]
 extern crate serde_json;
 
-use std::convert::Infallible;
 use std::collections::HashMap;
 use std::net::IpAddr;
 
-use rocket::http::HeaderMap;
+use rocket::http::{HeaderMap, Status};
 use rocket::request::{FromRequest, Outcome, Request};
 use rocket::serde::json::Value;
 
-struct RequestHeaders<'a> {
+struct APIRequest<'a> {
     headers: HeaderMap<'a>,
     ip: IpAddr,
 }
 
+#[derive(Debug)]
+enum RequestError {
+    IpNotFound,
+}
+
 #[rocket::async_trait]
-impl<'r> FromRequest<'r> for RequestHeaders<'r> {
-    type Error = Infallible;
+impl<'r> FromRequest<'r> for APIRequest<'r> {
+    type Error = RequestError;
 
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let ip = req.client_ip().unwrap();
-        Outcome::Success(RequestHeaders {
-            headers: req.headers().clone(),
-            ip: ip,
-        })
+        if let Some(client_ip) = req.client_ip() {
+            println!("Client IP: {}", client_ip);
+            Outcome::Success(APIRequest {
+                headers: req.headers().clone(),
+                ip: client_ip,
+            })
+        } else {
+            Outcome::Failure((Status::Unauthorized, Self::Error::IpNotFound))
+        }
     }
 }
 
 #[get("/")]
-fn index(token: RequestHeaders) -> Value {
-    let mut headers_map = HashMap::new();
-
-	let headers = token.headers.clone();
-    let _ = headers
+fn index(request: APIRequest) -> Value {
+    let mut headers_map: HashMap<String, String> = request
+        .headers
+        .clone()
         .into_iter()
         .map(|header| {
-            let header_str = header.name.as_str();
-            match header_str {
-                "user-agent" => {
-                    headers_map.insert("software", token.headers.get_one(header_str).unwrap());
-                }
-                "accept-language" => {
-                    headers_map.insert("language", token.headers.get_one(header_str).unwrap());
-                }
-                "x-forwarded-for" | "x-real-ip" => {
-                    headers_map.insert("ipaddress", token.headers.get_one(header_str).unwrap());
-                }
-                _ => (),
-            }
+            (
+                header.name.as_str().to_string(),
+                request
+                    .headers
+                    .get_one(header.name.as_str())
+                    .unwrap()
+                    .to_string(),
+            )
         })
-        .collect::<Vec<_>>();
+        .collect();
 
-	let ip_string = token.ip.to_string();
-    headers_map.insert("ipaddress", &ip_string);
+    headers_map.insert("ipaddress".to_string(), request.ip.to_string());
 
     json!(headers_map)
 }
